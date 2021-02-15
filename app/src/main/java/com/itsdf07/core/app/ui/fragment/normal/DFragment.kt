73aa5859228646e9ebc34184d93573b875fc898f
itsdf07 <923255742@qq.com>
2021-02-15 15:22:33 +0800

    增加首页导航栏tabbar

---
+++ app/src/main/java/com/itsdf07/core/app/ui/fragment/normal/DFragment.kt
@@ -0,0 +1,26 @@
+package com.itsdf07.core.app.ui.fragment.normal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.itsdf07.core.app.R
+
+/**
+ * @Description:
+ * @Author itsdf07
+ * @E-Mail 923255742@qq.com
+ * @Github https://github.com/itsdf07
+ * @Date 2021/2/14
+ */
+class DFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view: View = inflater.inflate(R.layout.fragment_d, null)
+        return view
+    }
+}
---
+++ app/src/main/java/com/itsdf07/core/app/ui/navibar/MainNaviBarActivity.kt
@@ -0,0 +1,81 @@
+package com.itsdf07.core.app.ui.navibar
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.itsdf07.core.app.R
+import com.itsdf07.core.app.common.widget.navibar.EasyNavigationBar
+import com.itsdf07.core.app.ui.fragment.normal.*
+import java.util.*
+
+/**
+ * @Description:
+ * @Author itsdf07
+ * @E-Mail 923255742@qq.com
+ * @Github https://github.com/itsdf07
+ * @Date 2021/2/14
+ */
+class MainNaviBarActivity : AppCompatActivity() {
+    lateinit var navigationBar: EasyNavigationBar
+    private val tabText =
+        arrayOf("首页", "我的")// "发现", "消息",
+
+    //未选中icon
+    private val normalIcon =
+        intArrayOf(R.mipmap.tabbar_main_normal, R.mipmap.tabbar_mine_normal)
+
+    //选中时icon
+    private val selectIcon =
+        intArrayOf(R.mipmap.tabbar_main_actived, R.mipmap.tabbar_mine_actived)
+    private val fragments: ArrayList<Fragment> = ArrayList()
+    private val mHandler = Handler()
+    private var flag = true
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navi_bar)
+        navigationBar = findViewById(R.id.navigationBar)
+        fragments.add(AFragment())
+        fragments.add(BFragment())
+//        fragments.add(CFragment())
+//        fragments.add(DFragment())
+//        fragments.add(EFragment())
+        navigationBar.titleItems(tabText)
+            .normalIconItems(normalIcon)
+            .selectIconItems(selectIcon)
+            .navigationHeight(50)
+            .tabTextSize(10)
+            .fragmentList(fragments)
+            .centerImageRes(R.mipmap.tabbar_add)
+            .centerTextStr("")
+//            .anim(null)
+            .centerLayoutRule(EasyNavigationBar.RULE_BOTTOM)
+            .centerLayoutBottomMargin(0)
+            .centerAlignBottom(true)
+            .fragmentManager(supportFragmentManager)
+            .setOnTabClickListener(object : EasyNavigationBar.OnTabClickListener {
+                override fun onTabSelectEvent(view: View?, position: Int): Boolean {
+                    return false
+                }
+
+                override fun onTabReSelectEvent(view: View?, position: Int): Boolean {
+                    return false
+                }
+            })
+            .setOnCenterTabClickListener {
+                mHandler.post(Runnable { //＋ 旋转动画
+                    if (flag) {
+                        navigationBar.centerImage.animate().rotation(45f).duration = 400
+                    } else {
+                        navigationBar.centerImage.animate().rotation(0f).duration = 400
+                    }
+                    flag = !flag
+                })
+                false
+            }
+            .canScroll(true)
+            .mode(EasyNavigationBar.NavigationMode.MODE_ADD)
+            .build()
+    }
+}
